5fdd5d570217a1be7a46fabe3cab538a10aa9199
Mendess2526 <pedro.mendes.26@gmail.com>
2019-11-07 15:28:31 +0000

    Reduce output

--- Assignment/docker_pgpool/keys/slave_replication.sh
+++ Assignment/docker_pgpool/keys/slave_replication.sh
@@ -2,6 +2,6 @@
 cd /var/lib/postgresql/9.3 || exit 1
 sudo -u postgres mv /var/lib/postgresql/base_backup.tar .
 sudo -u postgres rm -rf main/
-sudo -u postgres tar -xvf base_backup.tar
+sudo -u postgres tar -xf base_backup.tar
 sudo -u postgres mkdir -p main/pg_xlog/archive_status
 sudo -u postgres cp /keys/slave_recovery.conf main/recovery.conf && /etc/init.d/postgresql start
